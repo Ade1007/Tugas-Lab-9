e67df92bc7891ff6b8eaa4ac1e2c2711c3bafb96
Ade Harapano <61915515+Ade1007@users.noreply.github.com>
2020-07-01 18:24:41 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pertemuan8_Tugas
+{
+    public abstract class Karyawan
+    {
+        public abstract string Nik { get; set; }
+        public abstract string Nama { get; set; }
+        public abstract double Gaji();
+    }
+}
---
+++ KaryawanTetap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pertemuan8_Tugas
+{
+    class KaryawanTetap : Karyawan
+    {
+        public override string Nik { get; set; }
+        public override string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+        public override double Gaji() { return GajiBulanan; }
+    }
+}
